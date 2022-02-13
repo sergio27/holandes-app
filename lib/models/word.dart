4e9ed830cce03e3019a994cb517abda6b266e857
Sergio Ibarra Alcalá <ibarra.sergio@gmail.com>
2022-02-13 12:30:40 -0700

    Add word model

---
+++ lib/models/word.dart
@@ -0,0 +1,11 @@
+
+class Word {
+  String spanish;
+  String dutch;
+  String category;
+  int level;
+
+  Word({required this.spanish, required this.dutch,
+    required this.category, required this.level});
+
+}
